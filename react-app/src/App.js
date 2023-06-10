9475afb28a8c3492982e77d5963fc83b3a863ffa
definitely92 <tngus5232@naver.com>
2023-06-10 20:56:38 +0900

    생성 기능 구현

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -34,14 +34,29 @@ function Nav(props) {
   </ol>
 </nav>
 }
+function Create(props) {
+  return <article>
+    <form onSubmit={event => {
+      event.preventDefault();
+      const title = event.target.title.value; //event가 일어난 form을 target으로 잡고 그 안에 있는 title의 value
+      const body = event.target.body.value;
+      props.onCreate(title,body);
+    }}>
+      <p><input type='text' name='title' placeholder='title'/></p>
+      <p><textarea name='body' placeholder='body'></textarea></p>
+      <p><input type='submit' value='create'></input></p>
+    </form>
+  </article>
+}
 function App() {
   const [mode,setMode] = useState('WELCOME');
   const [id,setId] = useState(null);
-  const topics = [
+  const [nextId,setNextId] = useState(4);
+  const [topics,setTopics] = useState([
     {id : 1, title:'html', body : 'html is...'},
     {id : 2, title:'css', body : 'css is...'},
     {id : 3, title:'javascript', body : 'js is...'}
-  ]
+  ]);
   let content = null;
   if(mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB"></Article>
@@ -55,6 +70,16 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+  } else if(mode === 'CREATE') {
+    content = <Create onCreate={(_title,_body) => {
+      const newTopic = {id:nextId, title:_title, body: _body};
+      const copyTopics = [...topics]; // topics 복제
+      copyTopics.push(newTopic); // 복제한topics에 새로 생성된 topic 넣기
+      setTopics(copyTopics); //topics와 복제한 topics가 다르면 컴포넌트를 다시 렌더링
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
   }
   return (
     <div>
@@ -66,6 +91,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <a href='/create' onClick={event => {
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
